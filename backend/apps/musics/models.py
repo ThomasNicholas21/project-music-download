ab5735600b59e5fb11cb69b7728ec3788896033a
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-09-29 22:15:59 -0300

    feat: implementing musics apps

--- backend/apps/musics/models.py
+++ backend/apps/musics/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import User
+from apps.genres.models import Genres
+
+class Musics(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    genres = models.ForeignKey(
+        Genres,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    music_name = models.CharField(
+        "Music Name",
+        max_length=128,
+    )
+    # TODO: Create a custom storage to protect musics from other users
+    music_file = models.FileField(
+        "Music File",
+    )
